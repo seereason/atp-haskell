25c328115219aca000dcd498f31530d2329bddb5
David Fox <dsf@seereason.com>
2015-10-14 12:03:33 -0700

    Export everything from the ATP module

--- src/ATP.hs
+++ src/ATP.hs
@@ -2,9 +2,34 @@ module ATP
     ( module Lib
     , module Pretty
     , module Formulas
+    , module Lit
+    , module Prop
+    , module PropExamples
+    , module DefCNF
+    , module DP
+    , module FOL
+    , module Skolem
+    , module Herbrand
+    , module Unif
+    , module Tableaux
+    , module Resolution
+    , module Prolog
+    , module Meson
     ) where
 
 import Lib
 import Pretty
 import Formulas
-
+import Lit hiding (Atom, T, F, Not)
+import Prop hiding (tests, Atom, nnf)
+import PropExamples hiding (tests, K)
+import DefCNF hiding (tests)
+import DP hiding (tests)
+import FOL hiding (tests, T, F, Not, And, Or, Imp, Iff)
+import Skolem hiding (tests)
+import Herbrand hiding (tests, test01, test02)
+import Unif hiding (tests)
+import Tableaux hiding (tests, K)
+import Resolution hiding (tests)
+import Prolog hiding (tests)
+import Meson hiding (tests, test01, test02)
